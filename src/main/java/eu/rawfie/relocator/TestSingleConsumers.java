81911a7f7114cf170257458088fddc15e5f1499c
kgerakos <kgerakos@di.uoa.gr>
2018-12-07 11:18:35 +0200

    test

---
+++ src/main/java/eu/rawfie/relocator/TestSingleConsumers.java
@@ -0,0 +1,11 @@
+package eu.rawfie.relocator;
+
+import eu.rawfie.relocator.kafka.consumers.SinglePartitionConsumer;
+
+public class TestSingleConsumers {
+
+	public static void main(String[] args) {
+		SinglePartitionConsumer single = new SinglePartitionConsumer("172.19.0.18:9092", "http://172.19.0.18:8081", "testSingle", "porto_Location", 3);
+	}
+
+}
